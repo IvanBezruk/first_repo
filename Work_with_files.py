48f43b0a6bc6b8d5c00b83ff47857616a2f918c0
Ivan Bezruk <ivanbezruk2022@gmail.com>
2025-05-27 01:34:53 +0300

    another_update

--- Work_with_files.py
+++ Work_with_files.py
@@ -31,8 +31,75 @@ while True:
 fh.close()
 """
 
+"""
 fh = open('test.txt', 'w')
 fh.write('first line\nsecond line\nthirdline')
 fh.close()
 
 fh = open('test.txt', 'r')
+while True:
+    line = fh.readline()
+    if not line:
+        break
+    print(line)
+
+fh.close()
+"""
+
+"""
+fh = open('test.txt', 'w')
+fh.write('first line\nsecond line\nthirdline')
+fh.close()
+
+fh = open("test.txt", "r")
+lines = [el.strip() for el in fh.readlines()]
+print(lines)
+
+fh.close()
+"""
+
+"""
+fh = open('test.txt', 'w+')
+fh.write('hello!')
+
+fh.seek(1)
+second = fh.read(1)
+print(second) #'e'
+
+fh.close()
+"""
+
+"""
+fh = open("test.txt", "w+")
+fh.write("hello!")
+
+position = fh.tell()
+print(position)
+
+fh.seek(1)
+position = fh.tell()
+print(position) #1
+
+fh.read(2)
+position = fh.tell()
+print(position)
+
+fh.close()
+"""
+"""
+with open("test.txt", "w") as fh:
+    fh.write("Some data")
+"""
+
+"""
+with open("test.txt", "w") as fh:
+    fh.write("first line\nsecond line\nthird line")
+
+with open("test.txt", "r") as fh:
+    lines = [el.strip() for el in fh.readlines()]
+
+print(lines)
+"""
+
+with open('raw_data.bin', 'wb') as fh:
+    fh.write(b'Hello world!')
