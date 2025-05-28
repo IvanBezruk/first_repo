8e46fe08b687f6e256549f446b36a29fb8d3d800
Ivan Bezruk <ivanbezruk2022@gmail.com>
2025-05-28 15:43:28 +0300

    Encryption

--- Work_with_files.py
+++ Work_with_files.py
@@ -101,5 +101,21 @@ with open("test.txt", "r") as fh:
 print(lines)
 """
 
+"""
 with open('raw_data.bin', 'wb') as fh:
     fh.write(b'Hello world!')
+"""
+
+s = "Hola!"
+
+utf8 = s.encode()
+print(f"UTF-8: {utf8}")
+
+utf16 = s.encode("utf-16")
+print(f"UTF-16: {utf16}")
+
+cp1251 = s.encode("cp1251")
+print(f"CP-1251: {cp1251}")
+
+s_from_utf16 = utf16.decode("utf-16")
+print(s_from_utf16 == s)
