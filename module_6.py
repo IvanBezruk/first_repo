60be9803de557bbf5696e31c6c10e2f9e52cf7bd
Iva <ivanbezruk2022@gmail.com>
2025-08-13 17:34:04 +0200

    changes

--- module_6.py
+++ module_6.py
@@ -1,3 +1,4 @@
+"""
 from collections import UserDict
 
 contacts = [
@@ -23,3 +24,56 @@ for customer in customers:
 print("---------------------------")
 for customer in customers:
     print(customer.email_info())
+
+"""    
+"""
+from collections import UserList
+
+class MyList(UserList):
+    def add_if_not_exist(self, item):
+        if item not in self.data:
+            self.data.append(item)
+
+my_list = MyList([1, 2, 3])
+print("Original list:", my_list)
+
+my_list.add_if_not_exist(3)
+my_list.add_if_not_exist(4)
+my_list.add_if_not_exist(5)
+
+print("Sum:", sum(my_list))
+
+"""
+
+from collections import UserList
+
+class UniqueList(UserList):
+    def __init__(self, data=None):
+        super().__init__(data)
+        self.duplicates = 0
+    
+    def append(self, item):
+        if item not in self.data:
+            self.data.append(item)
+        else:
+            self.duplicates += 1
+    
+    def extend(self, items):
+        for item in items:
+            if item not in self.data:
+                self.data.append(item)
+            else:
+                self.duplicates += 1
+    
+    def get_duplicates_attempted(self):
+        return self.duplicates
+
+unique_list = UniqueList([1, 2, 3])
+print("Initial:", unique_list)
+
+unique_list.append(2)  # Should be blocked (duplicate)
+unique_list.append(4)  # Should be added
+unique_list.extend([3, 5, 2, 6])  # Only 5 and 6 should be added
+
+print("Final:", unique_list)
+print("Duplicates blocked:", unique_list.get_duplicates_attempted())
