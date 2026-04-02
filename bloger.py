9890856786623d916c247738a5d90b1943fca948
Ivan <ivanbezruk2022@gmail.com>
2026-04-02 12:39:58 +0200

    update_4

--- bloger.py
+++ bloger.py
@@ -121,4 +121,60 @@ print(is_level_up(150, 60))
 print(is_level_up(10, 60))
 """
 
+"""
+my_list = list(range(5))
 
+while my_list:
+    element = my_list.pop()
+    print(f"element: {element}")
+
+print(my_list)
+"""
+"""
+while True:
+    print("Inifinite loop")
+"""
+""" how to quit infinite loop
+while True:
+    answer = input("Enter a number:")
+    if answer == "quit":
+        break
+    print(f"You entered: {answer}")
+"""
+
+import random
+
+HEADS = "heads"
+TAILS = "tails"
+COIN_VALUES = [HEADS, TAILS]
+
+def flip_coin():
+    return random.choice(COIN_VALUES)
+
+def play_martingale(*, starting_fund: int, min_bet: int, max_bet: int) -> int:
+    steps_to_loose = 0
+    current_funds = starting_fund
+    current_bet = min_bet
+
+    while current_funds > 0:
+        print("==========") # To see different cycles of game
+        steps_to_loose +=1
+        current_funds -= current_bet
+        print(f"{current_funds=}, {current_bet=}")
+        flipped_coin_value = flip_coin()
+        
+        if flipped_coin_value ==HEADS:
+            win = current_bet * 2
+            print(f"{win=}")
+            current_funds += win
+            current_bet = min_bet
+        else:
+            print("loose")
+            current_bet *=2
+            if current_bet > max_bet:
+                current_bet = min_bet
+            if current_bet > current_funds:
+                current_bet = current_funds
+    return steps_to_loose
+
+print(play_martingale(starting_fund = 1000, min_bet =1, max_bet =100))
